a83d2cb776e43a577cf54a31d518f11529c40073
integerJI <jjs9536@gmail.com>
2021-12-26 23:24:12 +0900

    First Project Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # devops
+aws ec2, Docker, Jenkins
+
+# 2021.12.26
+* First Project Commit
---
+++ project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+import app.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', app.views.index, name='index'),
+    path('app/', include('app.urls')),
+]
